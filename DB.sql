b2ce26a3cdeb9f612c262cb3285ab69eb28ee18c
Anna Volkova <anotherwaygirl@yandex.ru>
2024-08-26 16:58:00 +0700

    DB changed

--- DB.sql
+++ DB.sql
@@ -22,17 +22,17 @@ CREATE TABLE IF NOT EXISTS Collections
     collection_title VARCHAR(60) NOT NULL
     collection_release_year YEAR NOT NULL
 
-CREATE TABLE IF NOT EXISTS Genre-Artist
+CREATE TABLE IF NOT EXISTS GenreArtist
     genreID INTEGER NOT NULL REFERENCES Genre(id)
     artistID INTEGER NOT NULL REFERENCES Artist(id)
     CONSTRAINT pk PRIMARY KEY (genreID, artistID)
 
-CREATE TABLE IF NOT EXISTS Albom-Artist
+CREATE TABLE IF NOT EXISTS AlbomArtist
     albomID INTEGER NOT NULL REFERENCES Albom(id)
     artistID INTEGER NOT NULL REFERENCES Artist(id)
     CONSTRAINT pk PRIMARY KEY (albomID, artistID)
 
-CREATE TABLE IF NOT EXISTS Track-Collections
+CREATE TABLE IF NOT EXISTS TrackCollections
     trackID INTEGER NOT NULL REFERENCES Track(id)
     collectionID INTEGER NOT NULL REFERENCES Collections(id)
     CONSTRAINT pk PRIMARY KEY (trackID, collectionID)
